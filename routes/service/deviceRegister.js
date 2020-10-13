4500515a1572edeb1e55ad5d96153cde25eddfd7
ydg8567 <ydg8567@naver.com>
2020-10-13 13:37:52 +0900

    get datas from deviceRegister

--- routes/service/deviceRegister.js
+++ routes/service/deviceRegister.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const router = express.Router();
+// const moment = require('moment');
 const connection = require("../connection");
 
 /* GET home page. */
+function currentTime() {
+    let date = new Date();
+    let current = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+
+    return current;
+}
 router.get('/', function(req, res) {
-    res.render('home/deviceRegister');
+    const getDeviceTypeQuery = `
+    SELECT distinct deviceType, deviceId
+    FROM finedust.device_manage
+    GROUP BY deviceType, deviceId
+    `
+
+    connection.query(getDeviceTypeQuery, function (err, rows) {
+        if(!err) {
+            res.render('home/deviceRegister', {
+                deviceData: rows,
+                currentTime: currentTime()
+            })
+        }
+        else {
+            res.render('home/deviceRegister', err);
+        }
+    })
 });
 
 module.exports = router;
