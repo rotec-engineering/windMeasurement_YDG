48251e0aeb784da508df58c5147d6cee147690ea
ydg8567 <ydg8567@naver.com>
2020-10-13 11:20:30 +0900

    make table that changing with deviceName

--- routes/service/deviceManage.js
+++ routes/service/deviceManage.js
@@ -22,4 +22,44 @@ router.get('/', function(req, res) {
     })
 });
 
+router.get('/api/search', (req, res) => {
+    const param = req.query;
+    const otherDeviceSearchQuery = `
+    SELECT deviceName, LEFT(registerDate, 10) AS registerDate, LEFT(modifyDate, 10) AS modifyDate, deviceType
+    FROM finedust.device_manage
+    WHERE deviceId = ${param.deviceId}
+    `
+    connection.query(otherDeviceSearchQuery, (err, rows) => {
+        if (!err) {
+            let result = null;
+            let modifyDate = rows[0].modifyDate === null ? '' : rows[0].modifyDate;
+
+            const html = `
+                <tr>
+                    <td colspan="2" style="border: 2px solid; height: 300px"> 이미지 
+                </td>
+                <tr>
+                    <th style="width: 30%; border: 1px solid"> 장치 타입 </th>
+                    <td style="width: 70%; border: 1px solid"> ${rows[0].deviceType} </td>
+                </tr>
+                <tr>
+                    <th style="width: 30%; border: 1px solid"> 등록 일 </th>
+                    <td style="width: 70%; border: 1px solid"> ${rows[0].registerDate} </td>
+                </tr>
+                <tr> 
+                    <th style="width: 30%; border: 1px solid"> 수정 일</th>
+                    <td style="width: 70%; border: 1px solid"> ${modifyDate} </td>
+                </tr>
+            `
+            result += html
+
+            res.send(result);
+        }
+        else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+})
+
 module.exports = router;
