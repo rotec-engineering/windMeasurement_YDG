2b3e0c95e379e52d1c4e914d77479af28f81b1f0
ydg8567 <ydg8567@naver.com>
2020-10-15 17:04:11 +0900

    update Img throw the deviceUpdate.pug

--- routes/service/deviceUpdate.js
+++ routes/service/deviceUpdate.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const connection = require("../connection");
+const fs =require('fs');
+const multer = require('multer');
+const storage = multer.diskStorage({                                                // set storage of multer
+    destination: (req, file, callback) => {                                             // file storage address
+        callback(null, "public/images/deviceImg/");
+    },
+    filename: (req, file, callback) => {                                                // file rename to originalName
+        callback(null, file.originalname);
+    }
+});
+const uploader = multer({storage: storage});                                      // upload img file on server
 
 function currentTime() {
     let date = new Date();
@@ -48,7 +59,7 @@ router.get('/api/update', (req, res) => {
     const param = req.query;
     const registerQuery = `
     UPDATE finedust.device_manage 
-    SET deviceName = '${param.deviceName}', deviceType = '${param.deviceType}', modifyDate = '${currentTime()}'
+    SET deviceName = '${param.deviceName}', deviceType = '${param.deviceType}', modifyDate = '${currentTime()}', imgLoadStatus = 'uploading'
     WHERE deviceId = ${param.deviceId}
     `;
 
@@ -62,5 +73,50 @@ router.get('/api/update', (req, res) => {
         }
     })
 })
+
+router.post('/api/imgUpload', uploader.single('deviceImg'), (req, res, next) => {
+    // console.log(req.file.filename);
+    const deviceImgSrcChangeQuery = `
+    SELECT deviceId
+    FROM finedust.device_manage
+    WHERE imgLoadStatus = 'uploading'
+    `
+
+    connection.query(deviceImgSrcChangeQuery, function (err, rows) {
+        const fileSrc = './public/images/deviceImg/';
+        const fileName = req.file === undefined ? '' : req.file.filename;                                               // if user didnt choose Img, the err would be happen
+        let deviceImgSrc = fileName === '' ? fileSrc + "notImg.jpg" : fileSrc + rows[0].deviceId + '_' + fileName;      // if user didnt choose Img
+
+        // kind of imgLoadStatus => 'uploading' & 'uploaded //
+        const deviceImgSrcInsertQuery = `
+        UPDATE finedust.device_manage
+        SET deviceImgSrc = '${deviceImgSrc}', imgLoadStatus = 'uploaded'
+        WHERE imgLoadStatus = 'uploading'
+        `;
+
+        if(!err) {
+            if(fileName !== '') {                                                                                       // not registered deviceImg
+                fs.rename(fileSrc + fileName, deviceImgSrc, function (err) {             // file rename (if I need, I could divide ImgFolder follow with users)
+                    if (err) throw err;
+                });
+            }
+
+            connection.query(deviceImgSrcInsertQuery, function(err, rows) {                                             // insert new imgSrc
+                if (!err) {
+                    const successMsg = "장치를 등록했습니다.";
+                    res.send(successMsg);
+                }
+                else {
+                    console.log(err);
+                    res.send(err);
+                }
+            })
+        }
+        else {
+            console.log("imageSrc Change Err");
+            return 0;
+        }
+    })
+});
 
 module.exports = router;
