780a178f2ea2713522140b873b193c73913fcc34
ydg8567 <ydg8567@naver.com>
2020-10-15 13:41:12 +0900

    image upload function

--- routes/service/deviceRegister.js
+++ routes/service/deviceRegister.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const connection = require("../connection");
+const multer = require('multer');
+const storage = multer.diskStorage({                                                // set storage of multer
+    destination: (req, file, callback) => {                                             // file storage address
+        callback(null, "public/images/deviceImg/");
+    },
+    filename: (req, file, callback) => {                                                // file rename to originalName
+        callback(null, file.originalname + Date.now());
+    }
+});
+const uploader = multer({storage: storage});                                      // upload img file on server
 
 function currentTime() {
     let date = new Date();
     let current = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 
     return current;
-}
+};
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -15,7 +26,7 @@ router.get('/', function(req, res) {
     SELECT distinct deviceType, deviceId
     FROM finedust.device_manage
     GROUP BY deviceType
-    `
+    `;
 
     connection.query(getDeviceTypeQuery, function (err, rows) {
         if(!err) {
@@ -27,25 +38,56 @@ router.get('/', function(req, res) {
         else {
             res.render('home/deviceRegister', err);
         }
-    })
+    });
 });
 
-router.get('/api/register', (req, res) => {
-    const param = req.query;
+router.post('/api/register', (req, res) => {
+    const param = req.body;
+    // kind of imgLoadStatus => 'uploading' & 'uploaded //
     const registerQuery = `
-    INSERT INTO finedust.device_manage (deviceName, deviceType)
-    VALUES ("${param.deviceName}", "${param.deviceType}")
-    `
+    INSERT INTO finedust.device_manage (deviceName, deviceType, imgLoadStatus)
+    VALUES ("${param.deviceName}", "${param.deviceType}", "uploading")
+    `;
+
+    if(param.deviceName === '' || param.deviceType === '') {                            // check the 'null' value from deviceName & deviceType
+        const errMsg = "장치 명 또는 자치 타입 입력이 필요합니다.";
+        res.send(errMsg)
+
+        return 0;
+    }
 
     connection.query(registerQuery, (err, rows) => {
         if (!err) {
-            res.send("data registration success");
+            const successMsg = "장치를 등록했습니다.";
+            res.send(successMsg);
+        }
+        else {
+            console.log(err);
+            res.send(err);
+        }
+    });
+});
+
+router.post('/api/imgUpload', uploader.single('deviceImg'), (req, res, next) => {
+    let fileSrc = 'public/images/deviceImg/';
+    console.log(fileSrc);
+    // kind of imgLoadStatus => 'uploading' & 'uploaded //
+    const deviceImgSrcInsertQuery = `
+    UPDATE finedust.device_manage
+    SET deviceImgSrc = CONCAT("${fileSrc}", deviceId), imgLoadStatus = 'uploaded'
+    WHERE imgLoadStatus = 'uploading'
+    `;
+
+    connection.query(deviceImgSrcInsertQuery, function(err, rows) {
+        if (!err) {
+            const successMsg = 'imgFile upload Success';
+            res.send(successMsg);
         }
         else {
             console.log(err);
             res.send(err);
         }
     })
-})
+});
 
 module.exports = router;
