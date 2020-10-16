0e3cfe7fbd7472c7bcb93dcae5eeac9ebf065b60
ydg8567 <ydg8567@naver.com>
2020-10-16 11:05:50 +0900

    unnecessary File delete

--- routes/mysqlConnection.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const mysql = require("mysql");
-const config = require('../config.json')
-
-const connection = mysql.createConnection(config[express().get('env')]);
-
-module.exports = connection;
