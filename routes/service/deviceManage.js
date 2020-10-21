0e48bf7b13e46d4abcde49aff8d061ae65e638ac
ydg8567 <ydg8567@naver.com>
2020-10-21 16:57:31 +0900

    clean code & remove "hide marker" function

--- routes/service/deviceManage.js
+++ routes/service/deviceManage.js
@@ -5,30 +5,42 @@ const connection = require("../connection");
 /* GET home page. */
 router.get('/', function(req, res) {
     const deviceSearchQuery = `
-    SELECT deviceName, LEFT(registerDate, 10) AS registerDate, LEFT(modifyDate, 10) AS modifyDate, deviceId, deviceType, deviceImgSrc
-    FROM finedust.device_manage
-    ORDER BY deviceName
+        SELECT deviceName, 
+            LEFT(registerDate, 10) AS registerDate, 
+            LEFT(modifyDate, 10) AS modifyDate, 
+            deviceId, 
+            deviceType, 
+            deviceImgSrc
+        FROM finedust.device_manage
+        ORDER BY deviceName
     `;
+
     connection.query(deviceSearchQuery, (err, rows) => {
         if(!err) {
             res.render('home/deviceManage', {
-                dataOfTable: rows,
-                deviceImgSrc: rows[0].deviceImgSrc.substring(9, rows[0].deviceImgSrc.length)
+                dataOfTable: rows,                                                                          // [registerDate, modifyDate, deviceId, deviceType, deviceImgSrc]
+                deviceImgSrc: rows[0].deviceImgSrc.substring(9, rows[0].deviceImgSrc.length)                // default path is 'public' So cut the front part
             })
         }
         else {
             console.log(err);
             res.render('home/deviceManage', err);
         }
-    })
+    });
 });
 
-router.get('/api/search', (req, res) => {
-    const param = req.query;
+router.get('/api/init', (req, res) => {
     const otherDeviceSearchQuery = `
-    SELECT deviceName, LEFT(registerDate, 10) AS registerDate, LEFT(modifyDate, 10) AS modifyDate, deviceType, deviceImgSrc, deviceLatitude, deviceLongitude, deviceId
-    FROM finedust.device_manage
-    `
+        SELECT deviceName, 
+            LEFT(registerDate, 10) AS registerDate, 
+            LEFT(modifyDate, 10) AS modifyDate, 
+            deviceId,
+            deviceType, 
+            deviceImgSrc, 
+            deviceLatitude, 
+            deviceLongitude
+        FROM finedust.device_manage
+    `;
 
     connection.query(otherDeviceSearchQuery, (err, rows) => {
         if (!err) {
@@ -38,17 +50,17 @@ router.get('/api/search', (req, res) => {
             console.log(err);
             res.send(err);
         }
-    })
-})
+    });
+});
 
 router.get('/api/delete', (req, res) => {
     const param = req.query;
-    console.log(param.deviceId);
     const deviceDeleteQuery = `
-    DELETE 
-    FROM finedust.device_manage
-    WHERE deviceId = ${param.deviceId}
-    `
+        DELETE 
+        FROM finedust.device_manage
+        WHERE deviceId = ${param.deviceId}
+    `;
+
     connection.query(deviceDeleteQuery, (err, rows) => {
         if (!err) {
             res.send("delete Success");
@@ -57,7 +69,7 @@ router.get('/api/delete', (req, res) => {
             console.log(err);
             res.send(err);
         }
-    })
-})
+    });
+});
 
 module.exports = router;
