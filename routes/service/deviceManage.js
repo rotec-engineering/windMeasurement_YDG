b8bcab45d12384b22151234a75cc15235d1353d7
ydg8567 <ydg8567@naver.com>
2020-10-22 13:11:50 +0900

    remove the Img from local storage

--- routes/service/deviceManage.js
+++ routes/service/deviceManage.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const connection = require("../connection");
+const fs =require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -42,7 +43,7 @@ router.get('/api/init', (req, res) => {
         FROM finedust.device_manage
     `;
 
-    connection.query(otherDeviceSearchQuery, (err, rows) => {
+    connection.query(otherDeviceSearchQuery, function (err, rows) {
         if (!err) {
             res.send(rows);
         }
@@ -54,19 +55,41 @@ router.get('/api/init', (req, res) => {
 });
 
 router.get('/api/delete', (req, res) => {
+    let deleteDeviceId;
     const param = req.query;
     const deviceDeleteQuery = `
         DELETE 
         FROM finedust.device_manage
         WHERE deviceId = ${param.deviceId}
     `;
+    const existingImgSrc = `
+        SELECT deviceImgSrc
+        FROM finedust.device_manage
+        WHERE deviceId = ${param.deviceId}
+    `;
 
-    connection.query(deviceDeleteQuery, (err, rows) => {
-        if (!err) {
-            res.send("delete Success");
+    connection.query(existingImgSrc, function (err, rows) {                                                 // get existing ImgSrc
+        if(!err) {
+            deleteDeviceId = rows[0].deviceImgSrc;
         }
         else {
-            console.log(err);
+            res.send("loadExistingImgSrc Err");
+        }
+    });
+
+    connection.query(deviceDeleteQuery, function (err) {
+        if (!err) {
+            if(deleteDeviceId !== '' || undefined){                                                         // if Img wasnt exist, not delete Img
+                fs.unlink(deleteDeviceId, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            }
+
+            res.send("device Delete Success");
+        }
+        else {
             res.send(err);
         }
     });
